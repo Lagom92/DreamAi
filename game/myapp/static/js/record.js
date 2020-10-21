3ac4face87c1bc5d38b2e7803d9fdbea66cac24a
Lagom92 <jyyt0147@gmail.com>
2020-10-21 16:11:41 +0900

    Study p5 js sound

--- game/myapp/static/js/record.js
+++ game/myapp/static/js/record.js
@@ -1,26 +1,55 @@
+var song;
+var amp;
+var button;
 let mic;
 
+var volhistory = [];
+
+// function toggleSong() {
+//   if (song.isPlaying()) {
+//     song.pause();
+//   } else {
+//     song.play();
+//   }
+// }
+
+// function preload() {
+//   song = loadSound('../static/sound/The-Avengers-Theme-Song.mp3');
+// }
+
 function setup() {
-  createCanvas(710, 200);
-
-  // 오디오 입력 생성하기
+  createCanvas(200, 200);
+  // button = createButton('toggle');
+  // button.mousePressed(toggleSong);
+  // song.play();
+  // amp = new p5.Amplitude();
   mic = new p5.AudioIn();
-
-  // 오디오 입력 시작하기
-  // 그 기본값은 .connect()(즉, 컴퓨터 스피커에 연결)되지 "않은" 상태입니다.
   mic.start();
 }
 
 function draw() {
-  background(200);
-
-  // 전체 볼륨(0과 1.0 사이) 받아오기
+  background(0);
   let vol = mic.getLevel();
+  // var vol = amp.getLevel();
   console.log(vol)
-  fill(127);
-  stroke(0);
+  volhistory.push(vol);
+  stroke(255);
+  noFill();
+  push();
+  var currentY = map(vol, 0, 1, height, 0);
+  translate(0, height / 2 - currentY);
+  beginShape();
+  for (var i = 0; i < volhistory.length; i++) {
+    var y = map(volhistory[i], 0, 1, height, 0);
+    vertex(i, y);
+  }
+  endShape();
+  pop();
+  if (volhistory.length > width - 50) {
+    volhistory.splice(0, 1);
+  }
 
-  // 마이크 소리의 볼륨에 따라 떠있는 높이가 변하는 타원 그리기
-  let h = map(vol, 0, 1, height, 0);
-  ellipse(width / 2, h - 25, 50, 50);
+  stroke(255, 0, 0);
+  line(volhistory.length, 0, volhistory.length, height);
+  // ellipse(100, 100, 200, vol * 200);
 }

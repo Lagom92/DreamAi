c352d41863592d72af2e4416ded059f0c5bde4e7
Lagom92 <jyyt0147@gmail.com>
2020-11-09 22:28:53 +0900

    Create cough audio predict

--- drf/xray/urls.py
+++ drf/xray/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('image/', views.predictImage, name="predictImage"),
-    path('image/<int:pk>/', views.detail, name="detail"),
+    path('image', views.predictImage, name="predictImage"),
+    path('image/<int:pk>', views.detail, name="detail"),
+
+    path('audio', views.predictAudio, name="predictAudio"),
+    path('audio/<int:pk>', views.detailAudio, name="detailAudio"),
 ]
--- drf/xray/views.py
+++ drf/xray/views.py
@@ -3,12 +3,16 @@ from rest_framework import serializers, status
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from .models import ChestXray
-from .serializers import ChestXraySerializer
-# from .ml.predict import predict_CXR
+from .models import ChestXray, CoughAudio
+from .serializers import ChestXraySerializer, CoughAudioSerializer
 from .apps import XrayConfig
+import matplotlib.pyplot as plt
+import numpy as np
+import librosa.display
+import librosa
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+# from .ml.predict import predict_CXR
 
 
 @api_view(['GET', 'POST'])
@@ -40,3 +44,48 @@ def detail(request, pk):
         serializer = ChestXraySerializer(xray, context={"request":request})
         return Response(serializer.data)
 
+
+@api_view(['GET', 'POST'])
+def predictAudio(request):
+    if request.method == 'GET':
+        queryset = CoughAudio.objects.all().order_by('-id')
+        serializer = CoughAudioSerializer(queryset, context={"request":request}, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CoughAudioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def detailAudio(request, pk):
+    cough = get_object_or_404(CoughAudio, pk=pk)
+    audio_path = cough.audio.path
+    # wav to jpg 
+    y, sr = librosa.load(audio_path, sr = None)
+    S = librosa.feature.melspectrogram(y, sr=22050, n_mels=128) 
+    S_DB = librosa.power_to_db(S, ref=np.max)
+    img = librosa.display.specshow(S_DB, sr=22050)
+    title = audio_path.split('\\')[-1][:-4]
+    image_path = './media/mel/'+ title +'.jpg'
+    # mel save
+    plt.savefig(image_path)
+    # cough audio predict
+    prediction = XrayConfig.predict_audio(image_path)
+    # save db
+    cough.mel = image_path[8:]
+    cough.prediction = prediction
+    cough.save()
+
+    context = {
+        "id": cough.id,
+        "audio": "/media/" + str(cough.audio),
+        "mel": "/media/" + str(cough.mel),
+        "created_at": cough.created_at,
+        "prediction": cough.prediction,
+    }
+
+    return Response(context)

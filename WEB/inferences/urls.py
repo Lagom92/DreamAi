a3281ebc6aedccd9f8cba48f18c21350d5b13a2d
Lagom92 <jyyt0147@gmail.com>
2020-11-23 02:07:33 +0900

    Create edit patient info

--- WEB/inferences/urls.py
+++ WEB/inferences/urls.py
@@ -6,5 +6,6 @@ app_name = 'inferences'
 urlpatterns = [
     path('', views.main, name='main'),
     path('patient/<int:pk>', views.detail, name='detail'),
+    path('patient/<int:pk>/edit', views.editInfo, name='editInfo'),
 
 ]
--- WEB/inferences/views.py
+++ WEB/inferences/views.py
@@ -22,4 +22,18 @@ def detail(request, pk):
     return render(request, 'detail.html', {'patient':patient})
 
 
-    
+'''환자 정보 수정 페이지'''
+@login_required
+def editInfo(request, pk):
+    patient = get_object_or_404(Patient, id=pk)
+    if request.user != patient.doctor:
+        return redirect('inferences:main')
+
+    if request.method == 'POST':
+        patient.prescription = request.POST['prescription']
+        patient.vitalSigns = request.POST['vitalSigns']
+        patient.history = request.POST['history']
+        patient.save()
+        return redirect('inferences:detail', pk)
+
+    return render(request, 'edit_info.html', {'patient':patient})

af072c8e1e27f9ff8935c0e2a3e59cb0c4e839ec
Lagom92 <jyyt0147@gmail.com>
2020-11-23 17:52:20 +0900

    Add predict multi

--- WEB/inferences/apps.py
+++ WEB/inferences/apps.py
@@ -7,51 +7,24 @@ class InferencesConfig(AppConfig):
     name = 'inferences'
 
     global label, img_size
-    label  = ('negative','positive')
     img_size = (224, 224)
+    label  = ('negative','positive')
 
     # Load model
-    global seg_model, feature_model, cxr_model, audio_model, multi_model
+    global seg_model, feature_model, cxr_model, multi_model
     ML_PATH = "./inferences/ml/"
 
     seg_model_name = ML_PATH + 'seg_model.h5'
     feature_model_name = ML_PATH + 'feature_model.h5'
     cxr_model_name = ML_PATH + 'cxr_model.h5'
-    # feature_model_name = ML_PATH + 'CXR_DenseNet_FP16_saved_model'
-    # cxr_model_name = ML_PATH + 'CXR_C2C_FP16_saved_model'
-    audio_model_name = ML_PATH + "audio_model.h5"
+    multi_model_name = ML_PATH + "multi_model"
 
     seg_model = tf.keras.models.load_model(seg_model_name,custom_objects={'dice_coef_loss': dice_coef_loss,'dice_coef':dice_coef})
     feature_model = tf.keras.models.load_model(feature_model_name)
     cxr_model = tf.keras.models.load_model(cxr_model_name)
-    # audio_model = tf.keras.models.load_model(audio_model_name)
+    multi_model = tf.keras.models.load_model(multi_model_name)
 
-    # multi_model = tf.keras.models.load_model(ML_PATH + "multi_model")
-
-    ''' CXR image predict function with TF-TRT '''
-    # def predict_CXR(image_path):
-    #     cropped_image = seg_modules.get_cropped_image(image_path, seg_model)
-    #     img = image_preprocessing(cropped_image, img_size)
-
-    #     cropped_img = tf.constant(img)
-
-    #     signature_keys = list(feature_model.signatures.keys())
-    #     infer = feature_model.signatures[signature_keys[0]]
-    #     pred = infer(cropped_img)
-    #     key = list(pred.keys())[0]
-    #     val = pred[key]
-
-    #     signature_keys = list(cxr_model.signatures.keys())
-    #     inference = cxr_model.signatures[signature_keys[0]]
-    #     prediction = inference(val)
-    #     key = list(prediction.keys())[0]
-    #     value = prediction[key].numpy()[0]
-
-    #     idx = int(np.round(value))
-            
-    #     return label[idx]
-
-
+    
     ''' CXR image predict function - with TF '''
     def predict_CXR(image_path):
         cropped_image = get_cropped_image(image_path, seg_model)
@@ -64,11 +37,14 @@ class InferencesConfig(AppConfig):
         return label[idx]
 
 
-    ''' Cough audio predict function with TF '''
-    def predict_audio(image_path):
-        img = audio_preprocessing(image_path, img_size)
-        feature_vector = feature_model.predict(img)
-        prediction = audio_model.predict(feature_vector)[0]
-        idx = int(prediction.round()[0])
+    ''' CXR and cough audio predict function with TF'''
+    def predict_multi(image_path, audio_mel_path):
+        cropped_image = get_cropped_image(image_path, apps.seg_model)
+        cxr_img = image_preprocessing(cropped_image, img_size) 
+        audio_img = audio_preprocessing(audio_mel_path, img_size)
+        image_feature_vector = apps.feature_model.predict(cxr_img)
+        audio_feature_vector = apps.feature_model.predict(audio_img)
+        prediction = apps.multi_model.predict([image_feature_vector, audio_feature_vector])[0]
+        predict = int(np.round(prediction))
 
-        return label[idx]
+        return label[predict]

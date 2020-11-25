cb24e8346687dd5cd57509f76b5728a8e0f5b617
Lagom92 <jyyt0147@gmail.com>
2020-11-25 16:03:57 +0900

    Create audio inference

--- WEB_audio/collection/apps.py
+++ WEB_audio/collection/apps.py
@@ -1,5 +1,31 @@
 from django.apps import AppConfig
+import tensorflow as tf
+from collection.predict import *
 
 
 class CollectionConfig(AppConfig):
     name = 'collection'
+
+    global label, img_size
+    label  = ('negative','positive')
+    img_size = (224, 224)
+
+    # Load model
+    global feature_model, audio_model
+    ML_PATH = "./collection/ml/"
+
+    feature_model_name = ML_PATH + 'feature_model.h5'
+    audio_model_name = ML_PATH + "audio_model.h5"
+
+    feature_model = tf.keras.models.load_model(feature_model_name)
+    audio_model = tf.keras.models.load_model(audio_model_name)
+
+
+    ''' Cough audio predict function with TF '''
+    def predict_audio(image_path):
+        img = audio_preprocessing(image_path, img_size)
+        feature_vector = feature_model.predict(img)
+        prediction = audio_model.predict(feature_vector)[0]
+        idx = int(prediction.round()[0])
+
+        return label[idx]
--- WEB_audio/collection/views.py
+++ WEB_audio/collection/views.py
@@ -1,7 +1,9 @@
+import collection
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 from .models import Cough
-
+from collection.apps import CollectionConfig
+from .predict import make_wav2img
 
 def main(request):
 
@@ -13,7 +15,7 @@ def audio(request):
     if request.method == 'POST':
         Cough.objects.create(
             audio = request.FILES['audio_data'],
-            name = str(request.FILES['audio_data'])
+            title = str(request.FILES['audio_data'])
         )
     
     return render(request, 'audio.html')
@@ -21,8 +23,12 @@ def audio(request):
 
 def infer(request):
     cough = Cough.objects.last()
+    audio_path = cough.audio.path
+    image_path = make_wav2img(audio_path)
+    prediction = CollectionConfig.predict_audio(image_path)
+    cough.mel = image_path[8:]
+    cough.prediction = prediction
+    cough.save()
 
-    infer = "cough test infer"
-
-    return render(request, 'infer.html', {'infer':infer})
+    return render(request, 'infer.html', {'prediction':prediction})
 
